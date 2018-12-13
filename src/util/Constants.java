bb0ef1f96f09399f0538af6bbae5fb712013fad3
xzsk2 <sk308269317@outlook.com>
2018-12-13 11:27:45 +0800

    Email account

---
+++ src/util/Constants.java
@@ -0,0 +1,8 @@
+package util;
+
+//Do not git it!
+//TODO：填入自己的邮箱账号密码，需要开启SMTP
+public class Constants {
+    public static final String myEmailAccount = "";
+    public static final String myEmailPassword = "";
+}
